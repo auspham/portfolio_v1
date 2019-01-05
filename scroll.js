19d8b4f335c26b858f60041849c010087ebb777e
AustinP <rockmanvnx6@gmail.com>
2019-01-06 01:14:03 +1100

    finally fixed typing + added scroll effect

--- scroll.js
+++ scroll.js
@@ -1,1 +1,10 @@
 window.addEventListener("scroll", scrollAnimate,false);
+function scrollAnimate() {
+    $(".H").css("transform", "translateY(-"+$(window).scrollTop()+"px)");
+    $(".e").css("transform", "translateY(-"+$(window).scrollTop()*.9+"px)");
+    $(".l").css("transform", "translateY(-"+$(window).scrollTop()*.7+"px)");
+    $(".l1").css("transform", "translateY(-"+$(window).scrollTop()*.9+"px)");
+    $(".o").css("transform", "translateY(-"+$(window).scrollTop()*.6+"px)");
+    $(".comma").css("transform", "translateY(-"+$(window).scrollTop()*.9+"px)");
+
+}
