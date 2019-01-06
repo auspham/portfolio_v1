37c209473f1c5e8abfd47e256b05e80b2e9c950f
AustinP <rockmanvnx6@gmail.com>
2019-01-06 17:01:37 +1100

    fix text scroll

--- scroll.js
+++ scroll.js
@@ -1,15 +1,15 @@
 window.addEventListener("scroll", scrollAnimate,false);
 function scrollAnimate() {
-    $(".H").css("transform", "translateY(-"+$(window).scrollTop()+"px)");
-    $(".e").css("transform", "translateY(-"+$(window).scrollTop()*.9+"px)");
-    $(".l").css("transform", "translateY(-"+$(window).scrollTop()*.7+"px)");
-    $(".l1").css("transform", "translateY(-"+$(window).scrollTop()*.9+"px)");
-    $(".o").css("transform", "translateY(-"+$(window).scrollTop()*1.2+"px)");
-    $(".comma").css("transform", "translateY(-"+$(window).scrollTop()*.9+"px)");
-    $(".A").css("transform", "translateY(-"+$(window).scrollTop()*.9+"px)");
-    $(".u").css("transform", "translateY(-"+$(window).scrollTop()*1+"px)");
-    $(".t").css("transform", "translateY(-"+$(window).scrollTop()*1.1+"px)");
-    $(".i").css("transform", "translateY(-"+$(window).scrollTop()*.75+"px)");
+    $(".H").css("transform", "translateY(-"+$(window).scrollTop()*1.7+"px)");
+    $(".e").css("transform", "translateY(-"+$(window).scrollTop()*1.6+"px)");
+    $(".l").css("transform", "translateY(-"+$(window).scrollTop()*1.8+"px)");
+    $(".l1").css("transform", "translateY(-"+$(window).scrollTop()*1.7+"px)");
+    $(".o").css("transform", "translateY(-"+$(window).scrollTop()*1.6+"px)");
+    $(".comma").css("transform", "translateY(-"+$(window).scrollTop()*1.6+"px)");
+    $(".A").css("transform", "translateY(-"+$(window).scrollTop()*1.8+"px)");
+    $(".u").css("transform", "translateY(-"+$(window).scrollTop()*1.6+"px)");
+    $(".t").css("transform", "translateY(-"+$(window).scrollTop()*1.5+"px)");
+    $(".i").css("transform", "translateY(-"+$(window).scrollTop()*1.5+"px)");
     $(".rocket").css("transform", "translateY(-"+$(window).scrollTop()*2+"px)");
     universeScale();
 }
