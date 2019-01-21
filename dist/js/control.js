6841c189f2f72d112af9c2d914b452bff907ba1b
AustinP <rockmanvnx6@gmail.com>
2019-01-21 20:42:17 +1100

    update control

--- dist/js/control.js
+++ dist/js/control.js
@@ -1,5 +1,6 @@
 var options = {
-    animateThreshold: 700,
+    animateThreshold: 400,
     scrollPollInterval: 20
 }
-$(".second .wrapper .description").AniView(options);
+$(".second .wrapper .description h1").AniView(options);
+$(".second .wrapper .description h4 , .btn").AniView(options);
