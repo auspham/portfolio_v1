3a5c81e533bf773c83704bfbdbc4e007c2652fcb
AustinP <rockmanvnx6@gmail.com>
2019-02-09 01:16:55 +1100

    js done

--- dist/js/control.js
+++ dist/js/control.js
@@ -4,3 +4,12 @@ var options = {
 }
 $(".second .wrapper .description h1").AniView(options);
 $(".second .wrapper .description h4").AniView();
+
+
+$(".menu .burger").click(function () {
+    $(".main-menu").css("display", "block");
+})
+
+$(".closebtn").click(function () {
+    $(".main-menu").css("display", "none");
+})
