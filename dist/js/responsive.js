96760edbbd37683a45983542384922516bc8da94
AustinP <rockmanvnx6@gmail.com>
2019-02-12 23:52:12 +1100

    Update responsive.js

--- dist/js/responsive.js
+++ dist/js/responsive.js
@@ -1,7 +1,7 @@
 window.addEventListener("resize", responsive, false);
 
 function responsive() {
-    if(window.innerWidth <= 600) {
+    if(window.innerWidth <= 700) {
         $(".scroll").addClass("d-scroll")
         $("h4").has("strong").css("width","inherit");
     }
