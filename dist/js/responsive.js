552a7121adffd4c13833b61613c4db7f3c4f88a8
AustinP <rockmanvnx6@gmail.com>
2019-03-08 14:47:57 +1100

    fix projects responsive

--- dist/js/responsive.js
+++ dist/js/responsive.js
@@ -4,10 +4,8 @@ function responsive() {
     if(window.innerWidth <= 700) {
         $(".scroll").addClass("d-scroll")
         $("h4").has("strong").css("width","inherit");
-        $(".project-content").attr("data-toggle", "tooltip");
-        $(".project-content").attr("data-placement", "top");
-        $(".project-content").attr("title", "Scroll for more");
     }
 }
 
 responsive();
+// alert(window.innerWidth+ ":" + window.innerHeight)
